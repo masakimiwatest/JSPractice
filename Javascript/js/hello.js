0f80b4c13315ddb6efc983b8a5173485e08d9e4e
test1 <42891920+masakimiwatest@users.noreply.github.com>
2020-07-06 13:26:50 +0900

    Add files via upload

---
+++ Javascript/js/hello.js
@@ -0,0 +1,3 @@
+// console.log("Hello World from hello.js");
+// console.log("Hello World from hello.js");
+// console.log("Hello World from hello.js");
---
+++ Javascript/js/variable.js
@@ -0,0 +1,11 @@
+ var a = "a";
+ console.log(a);
+
+var piro ="ピロちゃん♪";
+var cute ="かわいいよ！";
+
+console.log(piro + cute);
+
+var name;
+var _name;
+var $name;
